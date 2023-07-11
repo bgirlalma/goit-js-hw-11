458fdeb169643db4aa4045dfcd78a1b86f03fa8c
Alina Akulina <akulina.alina1999@gmail.com>
2023-07-11 20:05:39 +0000

    Homework №11

--- src/js/markup.js
+++ src/js/markup.js
@@ -12,7 +12,7 @@ export function markup(data) {
       }) =>
         `<div class="photo-card">
   <a href="${largeImageURL}">
-    <img class="image" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+    <img class="image" src="${webformatURL}" alt="${tags}" loading="lazy" width="300"/>
     <div class="info">
       <p class="info-item">
         <b>Likes: ${likes}</b>
